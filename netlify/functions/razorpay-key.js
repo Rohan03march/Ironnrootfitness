014a895c82eea4bdb96ea129054d7d2c96b7ae9e
Rohan.P <87313706+Rohan03march@users.noreply.github.com>
2025-09-09 22:28:58 +0530

    commit

--- netlify/functions/razorpay-key.js
+++ netlify/functions/razorpay-key.js
@@ -1,8 +1,8 @@
-export async function handler(event, context) {
+export async function handler() {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      key: process.env.RAZORPAY_KEY_ID
-    })
+      key: process.env.RAZORPAY_KEY_ID, // will come from Netlify env
+    }),
   };
 }

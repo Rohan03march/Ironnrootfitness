5d6ac47dfb732e42dabdfaa7281d6602b0bfe51d
Rohan.P <87313706+Rohan03march@users.noreply.github.com>
2025-09-11 00:42:49 +0530

    commit

--- netlify/functions/stripe-publishable-key.js
+++ netlify/functions/stripe-publishable-key.js
@@ -2,6 +2,7 @@
 exports.handler = async function() {
   return {
     statusCode: 200,
+    headers: {'Content-type': 'application/json'},
     body: JSON.stringify({ key: process.env.STRIPE_PUBLISHABLE_KEY }),
   };
 };

491c633d55ede6817cfbc28f150212b5b4d653be
Rohan.P <87313706+Rohan03march@users.noreply.github.com>
2025-09-11 02:15:34 +0530

    commit

--- js/pnp.js
+++ js/pnp.js
@@ -7,7 +7,15 @@ import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
 // Firebase config
-const firebaseConfig = { /* your config */ };
+const firebaseConfig = {
+  apiKey: "AIzaSyC1705Xy74qwXt8aOgvZGBIYs8uMU6u3js",
+  authDomain: "ironnrootfitness-5156e.firebaseapp.com",
+  projectId: "ironnrootfitness-5156e",
+  storageBucket: "ironnrootfitness-5156e.firebasestorage.app",
+  messagingSenderId: "508351386284",
+  appId: "1:508351386284:web:289185a2ff7a08b8ef0509",
+  measurementId: "G-S6235MZEPC"
+};
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);

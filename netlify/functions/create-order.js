fc2da641a70cce9d3494b2b16c5f7ab5f351e5dd
Rohan.P <87313706+Rohan03march@users.noreply.github.com>
2025-09-16 20:56:39 +0530

    commit

--- netlify/functions/create-order.js
+++ netlify/functions/create-order.js
@@ -13,7 +13,9 @@ exports.handler = async function(event, context) {
     const order = await instance.orders.create({
       amount,
       currency,
-      payment_capture: 1
+      payment_capture: 1,
+      //Offer only
+      //offer_id: "offer_RIC0zk7e73bDns",
     });
 
     return {
@@ -27,39 +29,3 @@ exports.handler = async function(event, context) {
     };
   }
 };
-
-
-
-/** For offer only  */
-
-// netlify/functions/create-order.js
-// const Razorpay = require("razorpay");
-
-// exports.handler = async function(event, context) {
-//   try {
-//     const instance = new Razorpay({
-//       key_id: process.env.RAZORPAY_KEY_ID,
-//       key_secret: process.env.RAZORPAY_KEY_SECRET
-//     });
-
-//     const { amount, currency } = JSON.parse(event.body);
-
-//     const order = await instance.orders.create({
-//       amount,
-//       currency,
-//       payment_capture: 1,
-//       offer_id: "offer_RIC0zk7e73bDns", // ✅ Add your live Offer ID here
-//     });
-
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify(order),
-//     };
-//   } catch (err) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: err.message }),
-//     };
-//   }
-// };
-

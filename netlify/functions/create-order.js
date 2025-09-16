a871da726f9a192c4423ca932251b10925cae6d2
Rohan.P <87313706+Rohan03march@users.noreply.github.com>
2025-09-16 13:16:58 +0530

    commit

--- netlify/functions/create-order.js
+++ netlify/functions/create-order.js
@@ -1,37 +1,3 @@
-// // netlify/functions/create-order.js
-// const Razorpay = require("razorpay");
-
-// exports.handler = async function(event, context) {
-//   try {
-//     const instance = new Razorpay({
-//       key_id: process.env.RAZORPAY_KEY_ID,
-//       key_secret: process.env.RAZORPAY_KEY_SECRET
-//     });
-
-//     const { amount, currency } = JSON.parse(event.body);
-
-//     const order = await instance.orders.create({
-//       amount,
-//       currency,
-//       payment_capture: 1
-//     });
-
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify(order),
-//     };
-//   } catch (err) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: err.message }),
-//     };
-//   }
-// };
-
-
-
-/** For offer only  */
-
 // netlify/functions/create-order.js
 const Razorpay = require("razorpay");
 
@@ -47,8 +13,7 @@ exports.handler = async function(event, context) {
     const order = await instance.orders.create({
       amount,
       currency,
-      payment_capture: 1,
-      offer_id: "offer_RIC0zk7e73bDns", // ✅ Add your live Offer ID here
+      payment_capture: 1
     });
 
     return {
@@ -62,4 +27,39 @@ exports.handler = async function(event, context) {
     };
   }
 };
+
+
+
+/** For offer only  */
+
+// netlify/functions/create-order.js
+// const Razorpay = require("razorpay");
+
+// exports.handler = async function(event, context) {
+//   try {
+//     const instance = new Razorpay({
+//       key_id: process.env.RAZORPAY_KEY_ID,
+//       key_secret: process.env.RAZORPAY_KEY_SECRET
+//     });
+
+//     const { amount, currency } = JSON.parse(event.body);
+
+//     const order = await instance.orders.create({
+//       amount,
+//       currency,
+//       payment_capture: 1,
+//       offer_id: "offer_RIC0zk7e73bDns", // ✅ Add your live Offer ID here
+//     });
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(order),
+//     };
+//   } catch (err) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: err.message }),
+//     };
+//   }
+// };
 

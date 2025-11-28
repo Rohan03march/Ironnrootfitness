6778d802f5cb395ce18ff6e312098afe7c3264c6
Rohan.P <87313706+Rohan03march@users.noreply.github.com>
2025-11-28 13:36:49 +0530

    commit

--- netlify/functions/create-order.js
+++ netlify/functions/create-order.js
@@ -14,7 +14,7 @@ exports.handler = async function(event, context) {
       amount,
       currency,
       payment_capture: 1,
-      offer_id: "offer_Rl5QpKTOWrYBkH",
+      //offer_id: "",  on in live mode...
     });
 
     return {

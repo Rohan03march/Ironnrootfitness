ee6543a048c9d36b7a1c805175833fe2acdc7893
Rohan.P <87313706+Rohan03march@users.noreply.github.com>
2026-01-18 17:43:00 +0530

    commit

--- js/pwp.js
+++ js/pwp.js
@@ -1,4 +1,4 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
